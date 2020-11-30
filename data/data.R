0aee3081dcaaabb524f40ad63b81880d1d293303
Stefan Fleck <stefan.fleck@statistik.gv.at>
2020-11-30 10:34:32 +0100

    add second example map

--- data/data.R
+++ data/data.R
@@ -5,15 +5,8 @@ library(gv.api.gvkapp)
 library(magrittr)
 
 
-examples_dir <- system.file("examples", package = "gv.api.gvkapp")
-list.files(examples_dir)
+# scp ***@localhost:/home/***/cache/dev/-6_1bfabe333efd149591868d0eb71f254f   /home/fleck/r-projekte/simple-webservices-with-plumber-uros2020/data/
 
-# scp sgvkapp@localhost:/home/sgvkapp/cache/dev/-5_0cd4f88bb4e4b9bf6c4692563091b301  /home/fleck/r-projekte/simple-webservices-with-plumber-uros2020/data/
-x <- readRDS(file.path(examples_dir, "2019-04-17_brunn-am-gebirge_klagenfurt-am-woerthersee.rds"))
+# -5_0cd4f88bb4e4b9bf6c4692563091b301
+# -6_1bfabe333efd149591868d0eb71f254f
 
-httr::GET()
-
-saveRDS(
-  x,
-  here("data/2019-04-17_brunn-am-gebirge_klagenfurt-am-woerthersee.rds")
-)
